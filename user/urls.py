dcadf31af77ed8103e102549ecf572e1ef6659f1
Badrinath Pandey <118422348+Badrinath960@users.noreply.github.com>
2025-01-02 11:20:45 +0530

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -12,9 +12,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'medical-centers', MedicalCenterViewSet)
-router.register(r'patients', PatientViewSet)
-router.register(r'donors', DonorViewSet)
-router.register(r'blood-requests', BloodRequestViewSet)
+# router.register(r'patients', PatientViewSet)
+# router.register(r'donors', DonorViewSet)
+# router.register(r'blood-requests', BloodRequestViewSet)
 router.register(r'regular-users', RegularUserViewSet)
 
 urlpatterns = [
